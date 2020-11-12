f1eafd7490b60c47aece5180ac1b4f173bc8af85
Rydge Craker <rcraker@menard-inc.com>
2020-11-12 16:19:49 -0600

    Fixing it

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDWebImage",
     platforms: [
-        .macOS(.v10_10),
-        .iOS(.v8),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
